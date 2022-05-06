1b2de52ce63c824732f3809dc3c78f8e3d97fe27
Nuppiz <nupethenuke@hotmail.com>
2022-05-06 20:56:37 +0300

    Bubble sort and binary search broken

--- Input.c
+++ Input.c
@@ -3,9 +3,16 @@
 #include <stdint.h>
 #include <string.h>
 
-#define TRUE    1
-#define FALSE   0
+#define TRUE    101
+#define FALSE   100
 #define STRING_MAX_LENGTH   21
+
+#define KEY_1   49
+#define KEY_2   50
+#define KEY_3   51
+#define KEY_4   52
+#define KEY_5   53
+#define KEY_6   54
 
 int running = 1;
 int string_count = 0;
@@ -44,20 +51,20 @@ void stringList()
     printf("\n%d string(s) saved.\n", string_count);
 }
 
-int stringCompare(char* searchString, char* compString)
+int stringMatch(char* string1, char* string2)
 {
     int comparison_char = 0;
     
-    while (searchString[comparison_char] != '\0' || compString[comparison_char] != '\0')
+    while (string1[comparison_char] != '\0' || string2[comparison_char] != '\0')
     {
-        if (searchString[comparison_char] != compString[comparison_char])
+        if (string1[comparison_char] != string2[comparison_char])
             return FALSE;
         comparison_char++;
     }
     return TRUE;
 }
 
-void stringSearch()
+void stringLinearSearch()
 {
     char search_input [STRING_MAX_LENGTH];
     int string_index;
@@ -69,7 +76,7 @@ void stringSearch()
     
     for (string_index = 0; string_index < string_count; string_index++)
     {
-        result = stringCompare(search_input, strings[string_index]);
+        result = stringMatch(search_input, strings[string_index]);
         if (result == TRUE)
         {
             match_string = string_index;
@@ -131,6 +138,79 @@ void stringBubbleSort()
     }
 }
 
+int stringCompare(char* string1, char* string2)
+{
+    int comparison_char = 0;
+    
+    while (string1[comparison_char] != '\0' || string2[comparison_char] != '\0')
+    {
+        if (string1[comparison_char] != string2[comparison_char])
+            return string1[comparison_char] - string2[comparison_char];
+        comparison_char++;
+    }
+    return 0;
+}
+
+int stringBinarySearch(char* search_string)
+{
+    int low = 0;
+    int high = string_count;
+    int mid;
+
+    char comp_result;
+
+    while (low <= high)
+    {
+        mid = low + (high - low) / 2;
+
+        printf("L:%d\n", low);
+        printf("M:%d\n", mid);
+        printf("H:%d\n", high);
+
+        if (search_string[0] == strings[mid][0])
+        {
+            comp_result = stringCompare(search_string, strings[mid]);
+
+            if (comp_result == 0)
+                return mid;
+            
+            else if (comp_result > 0)
+                low = mid + 1;
+
+            else
+                high = mid + 1;
+        }
+
+        else if (strings[mid][0] < search_string[0])
+            low = mid + 1;
+
+        else
+            high = mid - 1;
+    }
+
+    return FALSE;
+}
+
+void stringBinarySearchMenu()
+{
+    char search_input [STRING_MAX_LENGTH];
+    int result;
+
+    printf("Enter a string to search from the list.\n");
+    fgets(search_input, STRING_MAX_LENGTH, stdin);
+
+    result = stringBinarySearch(search_input);
+
+    if (result == FALSE)
+        printf("No matching strings found.\n");
+
+    else
+    {
+        printf("Found a result matching the search:\n");
+        printf("%s\n", strings[result]);
+    }
+}
+
 void menu()
 {
     char selection;
@@ -138,9 +218,10 @@ void menu()
     printf("-----------------------------\n");
     printf("Enter A to input a new string,\n");
     printf("B to list currently saved strings,\n");
-    printf("C to search for a string in the current list,\n");
+    printf("C to search for a string in the current list (linear),\n");
     printf("D to swap two strings on the list,\n");
-    printf("or E to sort the strings on the list.\n");
+    printf("E to sort the strings on the list,\n");
+    printf("or F to search for a string in the current list (binary).\n");
     
     scanf(" %c", &selection);
     if (selection == 'A')
@@ -153,7 +234,7 @@ void menu()
     else if (selection == 'C')
     {
         fflush (stdin);
-        stringSearch();
+        stringLinearSearch();
     }
     else if (selection == 'D')
     {
@@ -162,6 +243,11 @@ void menu()
     }
     else if (selection == 'E')
         stringBubbleSort();
+    else if (selection == 'F')
+    {
+        fflush (stdin);
+        stringBinarySearchMenu();
+    }     
 }
 
 int main()
